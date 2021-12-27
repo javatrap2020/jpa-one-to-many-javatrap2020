d78406ef2e0299141df860a37c21c0ad879bdee6
Oleh Boiko <javatrap2020@gmail.com>
2021-12-27 17:08:48 +0200

    jpa one to many 001

---
+++ src/main/java/com/example/jpaonetomanyjavatrap2020/JpaOneToManyJavatrap2020Application.java
@@ -0,0 +1,13 @@
+package com.example.jpaonetomanyjavatrap2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOneToManyJavatrap2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaOneToManyJavatrap2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpaonetomanyjavatrap2020/JpaOneToManyJavatrap2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaonetomanyjavatrap2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaOneToManyJavatrap2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
