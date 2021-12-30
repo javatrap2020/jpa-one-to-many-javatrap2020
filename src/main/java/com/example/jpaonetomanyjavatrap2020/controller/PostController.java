d89832fa77f0ea2a515b8d18cde1c0d11235f108
Oleh Boiko <javatrap2020@gmail.com>
2021-12-30 10:54:54 +0200

    add PostController and exception

---
+++ src/main/java/com/example/jpaonetomanyjavatrap2020/controller/PostController.java
@@ -0,0 +1,54 @@
+package com.example.jpaonetomanyjavatrap2020.controller;
+
+import com.example.jpaonetomanyjavatrap2020.exception.ResourceNotFoundException;
+import com.example.jpaonetomanyjavatrap2020.model.Post;
+import com.example.jpaonetomanyjavatrap2020.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/posts")
+    public Page<Post> getAllPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+
+    @PostMapping("/posts")
+    public Post createPost(@Valid @RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @PutMapping("/posts/{postId}")
+    public Post updatePost(@PathVariable Long postId, @Valid @RequestBody Post postRequest) {
+        return postRepository.findById(postId).map(post -> {
+            post.setTitle(postRequest.getTitle());
+            post.setDescription(postRequest.getDescription());
+            post.setContent(postRequest.getContent());
+            return postRepository.save(post);
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId" + postId + " not found"));
+    }
+
+    public ResponseEntity<?> deletePost(@PathVariable Long postId) {
+        return postRepository.findById(postId).map(post -> {
+            postRepository.delete(post);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+    }
+
+
+
+}
