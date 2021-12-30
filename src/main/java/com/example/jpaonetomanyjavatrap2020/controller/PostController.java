bcb53d1cdd7f5c4bf6fa681cfd8bcfcba515cd71
Oleh Boiko <javatrap2020@gmail.com>
2021-12-30 10:57:08 +0200

    add anotation @DeletMapping

--- src/main/java/com/example/jpaonetomanyjavatrap2020/controller/PostController.java
+++ src/main/java/com/example/jpaonetomanyjavatrap2020/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +43,7 @@ public class PostController {
         }).orElseThrow(() -> new ResourceNotFoundException("PostId" + postId + " not found"));
     }
 
+    @DeleteMapping("/posts/{postId}")
     public ResponseEntity<?> deletePost(@PathVariable Long postId) {
         return postRepository.findById(postId).map(post -> {
             postRepository.delete(post);
